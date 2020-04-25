e66fd482a4543e777c2642bf654816a405061fd2
michael <juy075@ucsd.edu>
2020-04-25 15:56:30 -0700

    Add MonkeyLearnClient

---
+++ jupiter/src/main/java/external/MonkeyLearnClient.java
@@ -0,0 +1,71 @@
+package external;
+
+import com.monkeylearn.ExtraParam;
+import com.monkeylearn.MonkeyLearn;
+import com.monkeylearn.MonkeyLearnResponse;
+import com.monkeylearn.MonkeyLearnException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class MonkeyLearnClient {
+	private static final String API_KEY = "7154ed6f2423a0bf279851fbef2a581e8850192a";
+	
+	public static void main( String[] args ) throws MonkeyLearnException {
+        String[] textList = {"import org. apache. http. client .methods. CloseableHttpResponse;"};
+        List<List<String>> words = extractKeywords(textList);
+		for (List<String> ws : words) {
+			for (String w : ws) {
+				System.out.println(w);
+			}
+			System.out.println();
+		}
+    }
+	
+	public static List<List<String>> extractKeywords(String[] text){
+		if (text == null || text.length == 0) {
+			return new ArrayList<>();
+		}
+
+		// Use the API key from your account
+		MonkeyLearn ml = new MonkeyLearn(API_KEY);
+
+		// Use the keyword extractor
+		ExtraParam[] extraParams = { new ExtraParam("max_keywords", "3") };
+		MonkeyLearnResponse response;
+		try {
+			response = ml.extractors.extract("ex_YCya9nrn", text, extraParams);
+			JSONArray resultArray = response.arrayResult;
+			return getKeywords(resultArray);
+		} catch (MonkeyLearnException e) {// it’s likely to have an exception
+			e.printStackTrace();
+		}
+		return new ArrayList<>();
+	}
+	
+	private static List<List<String>> getKeywords(JSONArray mlResultArray) {
+		List<List<String>> topKeywords = new ArrayList<>();
+		// Iterate the result array and convert it to our format.
+		// i represents job descriptions
+		for (int i = 0; i < mlResultArray.size(); ++i) {
+			List<String> keywords = new ArrayList<>();
+			JSONArray keywordsArray = (JSONArray) mlResultArray.get(i);
+			// every job description have n(<= 3) keywords
+			for (int j = 0; j < keywordsArray.size(); ++j) {
+				JSONObject keywordObject = (JSONObject) keywordsArray.get(j);
+				// We just need the keyword, excluding other fields.
+				//Java API use "keyword", RESTFUL API use "parse_value"
+				String keyword = (String) keywordObject.get("keyword");
+				keywords.add(keyword);
+
+			}
+			topKeywords.add(keywords);
+		}
+		return topKeywords;
+	}
+
+
+}
